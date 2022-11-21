87e412f15398788bcaacd36f4e598bfb85d9ee2f
julian <julian.osw04@gmail.com>
2022-11-21 10:34:04 +0100

    Add connection to database, add comments

--- Server/wetterserver/src/main/java/de/wetterstation/WetterServer.java
+++ Server/wetterserver/src/main/java/de/wetterstation/WetterServer.java
@@ -12,21 +12,31 @@ public class WetterServer {
     private ServerSocket serverSocket;
     private boolean stopping = false;
 
+    /**
+     * Starts the thread to listen for inputs in the server console
+     * Starts listening for incoming connections and handles them
+     * The socket is handled in a different thread (SocketThread.java)
+     * @param port the port the server should listen for incoming connections on
+     */
     public WetterServer(int port) {
         try {
             System.out.println("Wetterstation Server\n");
             this.serverSocket = new ServerSocket(port);
             System.out.println("Starte Server..");
 
+            // start thread for messages from console
             new ScannerThread(this).start();
 
             System.out.println("Warte auf eingehende Verbindung..");
             while (!this.stopping) {
+                // accept a client
                 Socket socket = this.serverSocket.accept();
 
+                // create and start a thread for a client
                 SocketThread socketThread = new SocketThread(socket);
                 socketThread.start();
                 try {
+                    // wait for the clientThread to stop
                     socketThread.join();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -39,6 +49,9 @@ public class WetterServer {
         }
     }
 
+    /**
+     * Stop the server
+     */
     public void stop() {
         try {
             this.serverSocket.close();
@@ -53,6 +66,10 @@ public class WetterServer {
         new WetterServer(1234);
     }
 
+    /**
+     * Change value of boolean stopping
+     * @param b new value for stopping
+     */
     public void setStopping(boolean b) {
         this.stopping = b;
     }
--- Server/wetterserver/src/main/java/de/wetterstation/thread/SocketThread.java
+++ Server/wetterserver/src/main/java/de/wetterstation/thread/SocketThread.java
@@ -1,15 +1,31 @@
 package de.wetterstation.thread;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import de.wetterstation.util.Socket;
 
 public class SocketThread extends Thread {
 
     private Socket socket;
+    private Connection connection;
 
     public SocketThread(Socket socket) {
         this.socket = socket;
+        try {
+            // connect to database
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "");
+            if (this.connection == null) {
+                System.out.println("Es konnte keine Verbindung zur Datenbank hergestellt werden!");
+                return;
+            }
+            System.out.println("Verbindung zur Datenbank hergestellt!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -19,15 +35,33 @@ public class SocketThread extends Thread {
 
             String line = "";
             while (true) {
+                // read line from client
                 line = socket.readLine();
                 if (line.equals("exit")) {
                     break;
                 }
                 double temperature = Double.parseDouble(line);
                 System.out.println("Lese " + temperature);
-                // Werte in DB eintragen
+
+                try {
+                    // write data into database
+                    PreparedStatement preparedStatement = this.connection
+                            .prepareStatement("INSERT INTO `daten` (`temp`) VALUES ('" + temperature + "')");
+                    preparedStatement.execute();
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
 
+            try {
+                // close connection to database
+                this.connection.close();
+                System.out.println("Verbindung zur Datenbank getrennt!");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            // close connection to client
             socket.close();
             System.out.println("Verbindung zur Wetterstation getrennt!");
             System.out.println("Versuche Verbindung wieder herzustellen!");
