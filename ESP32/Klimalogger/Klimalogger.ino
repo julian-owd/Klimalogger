ffaf1076be4c6bde8cf3cb31ecc5637b32f49ee7
Paul Aßmann <assmann.paul@gmx.de>
2022-12-12 10:34:24 +0100

    Display + LED
    - Fehlermeldungen auf Deutsch
    - LEDs hinzugefügt
    - Fehlermeldungen hinzugefügt

--- ESP32/Klimalogger/Klimalogger.ino
+++ ESP32/Klimalogger/Klimalogger.ino
@@ -10,18 +10,42 @@
 #define CO2PIN 19
 #define DHTTYPE DHT22
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);  
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
+
+int ledRotTemp = 34;
+int ledRotHum = 33;
+int ledRotCO2 = 27;
+
+int ledGelbTemp = 35;
+int ledGelbHum = 25;
+int ledGelbCO2 = 14;
+
+int ledGruenTemp = 32;
+int ledGruenHum = 26;
+int ledGruenCO2 = 12;
 
 const char* ssid = "JulianHotspot"; // Name of Wifi
-const char* password = "09022004"; // Password of Wifi
+const char* password = "12345678neun"; // Password of Wifi
 String HOST = "http://192.168.219.177:80/wetter/insert_data.php"; // IP des Geräts mit der Datenbank im lokalen Netzwerk
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
     Serial.begin(115200);
     delay(1000);
+
     pinMode(2, OUTPUT); // ONBOARD LED
     pinMode(CO2PIN, INPUT);
+
+    pinMode(ledRotTemp,OUTPUT);
+    pinMode(ledGelbTemp,OUTPUT);
+    pinMode(ledGruenTemp,OUTPUT);
+    pinMode(ledRotHum,OUTPUT);
+    pinMode(ledGelbHum,OUTPUT);
+    pinMode(ledGruenHum,OUTPUT);
+    pinMode(ledRotCO2,OUTPUT);
+    pinMode(ledGelbCO2,OUTPUT);
+    pinMode(ledGruenCO2,OUTPUT);
+
 
     lcd.init();
     // turn on LCD backlight                      
@@ -34,9 +58,12 @@ void setup() {
 
     while(WiFi.status() != WL_CONNECTED) {
       Serial.print(".");
-      lcd.print("Not connected");
-        delay(500);
-        lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("WLAN Verbindung");
+      lcd.setCursor(0, 1);
+      lcd.print("wird hergestellt");
+      delay(500);
+      lcd.clear();
     }
 
     digitalWrite(2, LOW);
@@ -72,9 +99,9 @@ void loop() {
 
         lcd.clear();
         lcd.setCursor(0, 0);
-        lcd.print("Failed to read");
+        lcd.print("Fehler");
         lcd.setCursor(0, 1);
-        lcd.print("from DHT sensor!"); 
+        lcd.print("DHT-Sensor"); 
                
         continue;
       }
@@ -84,9 +111,9 @@ void loop() {
 
         lcd.clear();
         lcd.setCursor(0, 0);
-        lcd.print("Failed to read");
+        lcd.print("Fehler");
         lcd.setCursor(0, 1);
-        lcd.print("CO2 data!"); 
+        lcd.print("CO2-Sensor"); 
         
         continue;
       }
@@ -105,6 +132,8 @@ void loop() {
       lcd.setCursor(0,1);
       lcd.print("CO2: "+String(co2 / values,0));
 
+      
+
       delay(2000);
     }
 
@@ -115,6 +144,70 @@ void loop() {
     if (humidity == 0 && temperature == 0 && co2 == 0) {
       return;
     }
+
+    digitalWrite(ledRotTemp, LOW);
+    digitalWrite(ledGelbTemp, LOW);
+    digitalWrite(ledGruenTemp, LOW);
+    digitalWrite(ledRotHum, LOW);
+    digitalWrite(ledGelbHum, LOW);
+    digitalWrite(ledGruenHum, LOW);
+    digitalWrite(ledRotCO2, LOW);
+    digitalWrite(ledGelbCO2, LOW);
+    digitalWrite(ledGruenCO2, LOW);
+
+
+//TEMP
+    if(temperature >= 20 && temperature <= 23){
+      digitalWrite(ledRotTemp, LOW);
+      digitalWrite(ledGelbTemp, LOW);
+      digitalWrite(ledGruenTemp, HIGH);
+    }
+    if(temperature >= 16 && temperature <= 19 || temperature >= 24 && temperature <= 26){
+      digitalWrite(ledRotTemp, LOW);
+      digitalWrite(ledGelbTemp, HIGH);
+      digitalWrite(ledGruenTemp, LOW);
+    }
+    if(temperature < 16 || temperature > 26){
+      digitalWrite(ledRotTemp, HIGH);
+      digitalWrite(ledGelbTemp, LOW);
+      digitalWrite(ledGruenTemp, LOW);
+    }
+
+//CO2
+    if(co2 < 1000){
+      digitalWrite(ledRotCO2, LOW);
+      digitalWrite(ledGelbCO2, LOW);
+      digitalWrite(ledGruenCO2, HIGH);
+    }
+    if(co2 >= 1000 && co2 <= 2000){
+      digitalWrite(ledRotCO2, LOW);
+      digitalWrite(ledGelbCO2, HIGH);
+      digitalWrite(ledGruenCO2, LOW);
+    }
+    if(co2 > 2000){
+      digitalWrite(ledRotCO2, HIGH);
+      digitalWrite(ledGelbCO2, LOW);
+      digitalWrite(ledGruenCO2, LOW);
+    }
+
+//HUMIDITY
+    if( humidity >= 40 && humidity <= 60){
+      digitalWrite(ledRotHum, LOW);
+      digitalWrite(ledGelbHum, LOW);
+      digitalWrite(ledGruenHum, HIGH);
+    }
+    if(humidity > 60  && humidity < 70 || humidity > 30 && humidity < 40){
+      digitalWrite(ledRotHum, LOW);
+      digitalWrite(ledGelbHum, HIGH);
+      digitalWrite(ledGruenHum, LOW);
+    }
+    if(humidity <= 30 || humidity >= 70){
+      digitalWrite(ledRotHum, HIGH);
+      digitalWrite(ledGelbHum, LOW);
+      digitalWrite(ledGruenHum, LOW);
+    }
+
+
     
     Serial.println("-------------------");
     Serial.print("Humidity: ");
@@ -131,10 +224,16 @@ void loop() {
     while (WiFi.status() != WL_CONNECTED) {
       WiFi.reconnect();
       Serial.println("Connection to WiFi lost");
+
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("WLAN Verbindung");
+      lcd.setCursor(0, 1);
+      lcd.print("wird hergestellt");
+
       digitalWrite(2, HIGH);
       delay(2000);
     }
-
 
     digitalWrite(2, LOW);
 
@@ -152,9 +251,21 @@ void loop() {
       } else {
         // HTTP header has been send and Server response header has been handled
         Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Web-Verbindung");
+        lcd.setCursor(0, 1);
+        lcd.print("fehlgeschlagen");
       }
     } else {
       Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Web-Verbindung");
+      lcd.setCursor(0, 1);
+      lcd.print("fehlgeschlagen");
     }
 
     http.end();
