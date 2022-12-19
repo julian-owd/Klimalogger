48c0a72a28291d13a141bb98c791a00ef3d8c788
Julian <julian.osw04@gmail.com>
2022-12-19 17:23:14 +0100

    Fix style

--- Website/data/export_data.php
+++ Website/data/export_data.php
@@ -2,37 +2,37 @@
  
 include_once '../db_config.php';
 
- // Fetch records from database 
- $query = $db->query("SELECT `label`, `created`, `temperature`, `humidity`, `co2` FROM `sensor` JOIN `room` ON room.id = sensor.rId ORDER BY `rId` ASC, `created` ASC;");
+// Fetch records from database 
+$query = $db->query("SELECT `label`, `created`, `temperature`, `humidity`, `co2` FROM `sensor` JOIN `room` ON room.id = sensor.rId ORDER BY `rId` ASC, `created` ASC;");
   
- if ($query->num_rows > 0) {
-     $delimiter = ",";
-     $filename = "klima-daten-".date('Y-m-d') . ".csv"; 
+if ($query->num_rows > 0) {
+    $delimiter = ",";
+    $filename = "klima-daten-".date('Y-m-d') . ".csv"; 
       
-     // Create a file pointer 
-     $f = fopen('php://memory', 'w'); 
+    // Create a file pointer 
+    $f = fopen('php://memory', 'w'); 
       
-     // Set column headers 
-     $fields = array('ROOM', 'CREATED', 'TEMPERATURE', 'HUMIDITY', 'CO2');
-     fputcsv($f, $fields, $delimiter); 
-      
-     // Output each row of the data, format line as csv and write to file pointer 
-     while ($row = $query->fetch_assoc()) { 
-         $lineData = array($row['label'], $row['created'], $row['temperature'], $row['humidity'], $row['co2']); 
-         fputcsv($f, $lineData, $delimiter); 
-     } 
+    // Set column headers 
+    $fields = array('ROOM', 'CREATED', 'TEMPERATURE', 'HUMIDITY', 'CO2');
+    fputcsv($f, $fields, $delimiter); 
      
-     // Move back to beginning of file 
-     fseek($f, 0); 
+    // Output each row of the data, format line as csv and write to file pointer 
+    while ($row = $query->fetch_assoc()) { 
+        $lineData = array($row['label'], $row['created'], $row['temperature'], $row['humidity'], $row['co2']); 
+        fputcsv($f, $lineData, $delimiter); 
+    } 
+    
+    // Move back to beginning of file 
+    fseek($f, 0); 
+     
+    // Set headers to download file rather than displayed 
+    header('Content-Type: text/csv'); 
+    header('Content-Disposition: attachment; filename="' . $filename . '";'); 
       
-     // Set headers to download file rather than displayed 
-     header('Content-Type: text/csv'); 
-     header('Content-Disposition: attachment; filename="' . $filename . '";'); 
-      
-     //output all remaining data on a file pointer 
-     fpassthru($f); 
- } 
- $db->close();
- exit; 
+    //output all remaining data on a file pointer 
+    fpassthru($f); 
+} 
+$db->close();
+exit; 
   
- ?>
+?>
